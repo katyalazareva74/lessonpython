fa47451763e44d4c1d59f4bb795c223d0040b91e
Ekaterina Lazareva <lazarevakatya@mail.ru>
2023-01-29 20:34:16 +0300

    Changes 1

--- Task006/main.py
+++ Task006/main.py
@@ -3,20 +3,19 @@
 # Примеры:
 # 385916 -> yes
 # 123456 -> no
+
 n = int(input('Введите номер вашего билета: '))
 sum = 0
 sum1 = 0
 n1 = n
 sum += n % 10
-n = n//10
+n //= 10
 sum += n % 10
-n = n//10
+n //= 10
 sum += n % 10
-n = n//10
+n //= 10
 sum1 += n % 10
-n = n//10
+n //= 10
 sum1 += n % 10 + n//10
-if(sum == sum1):
-    print(n1, '-> yes')
-else:
-    print(n1, '-> no')
+
+print(n1, '-> ', sum == sum1 and 'yes' or 'no')
